28b445999de067895a6eddc67c2c866a2d30fe33
prevail98 <prevailoyedepo5@gmail.com>
2023-04-26 14:43:42 -0700

    testing3

--- _printf.c
+++ _printf.c
@@ -1,89 +1,66 @@
+#include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
 
 /**
- * print_c - Prints a character to stdout.
- * @args: The arguments passed to the function.
- * @count: The number of characters printed.
+ * print_format - parses and prints a format string
+ * @format: the format string to print
+ * @args: the list of arguments to print
  *
- * Return: The updated count.
+ * Return: the number of characters printed
  */
-int print_c(va_list args, int count)
+int print_format(const char *format, va_list args)
 {
-	char c;
+	int i, j, k, count;
+	var_t type[] = {
+		{"c", c_func}, {"s", s_func}, {"i", i_func}, {"%", perc_func},
+		{"d", d_func},	{"b", b_func},	{"r", rev_func}, {"R", rot_func},
+		{NULL, NULL},
+	};
 
-	c = va_arg(args, int);
-	putchar(c);
-	count++;
-
-	return (count);
-}
-
-/**
- * print_s - Prints a string to stdout.
- * @args: The arguments passed to the function.
- * @count: The number of characters printed.
- *
- * Return: The updated count.
- */
-int print_s(va_list args, int count)
-{
-	const char *s;
-
-	for (s = va_arg(args, const char *); *s; s++)
+	i = 0, count = 0, k = 0;
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	while (format && format[i])
 	{
-		putchar(*s);
-		count++;
+		if (format[i] != '%')
+			_putchar(format[i]), k++;
+		else
+		{
+			j = 0;
+			while (type[j].vartype)
+			{
+				if (format[i + 1] == *type[j].vartype)
+				{
+					count += (type[j].f)(args), i++;
+					break;
+				}
+				j++;
+			}
+			if (type[j].vartype == NULL)
+				count += 1, _putchar('%');
+		}
+		i++;
 	}
-
-	return (count);
+	k += count;
+	return (k);
 }
 
 /**
- * _printf - Prints output according to a format.
- * @format: The format string.
+ * _printf - function that produces output according to a format
+ * @format: type of argument passed to function
  *
- * Return: The number of characters printed.
+ * Return: the number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i, count = 0;
+	int k;
 
 	va_start(args, format);
-
-	for (i = 0; format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			switch (format[++i])
-			{
-			case 'c':
-				count = print_c(args, count);
-				break;
-			case 's':
-				count = print_s(args, count);
-				break;
-			case '%':
-				putchar('%');
-				count++;
-				break;
-			default:
-				putchar('%');
-				putchar(format[i]);
-				count += 2;
-				break;
-			}
-		}
-		else
-		{
-			putchar(format[i]);
-			count++;
-		}
-	}
-
+	k = print_format(format, args);
 	va_end(args);
 
-	return (count);
+	return (k);
 }
 
